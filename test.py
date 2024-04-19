7c93cbdfa625fc9b2d435ff23a7e0bf0f528b843
rafawang2 <rafawcy@gmail.com>
2024-04-19 18:42:09 +0800

    test.py: 用訓練出的模型對valid data做預測

---
+++ test.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+model = YOLO("runs\\detect\\train\\weights\\best.pt")
+if __name__ == '__main__':
+    result = model.predict(
+        source="dataset/valid/images",
+        mode="predict",
+        save=True,
+        device="0"
+    )
--- yolov8.py
+++ yolov8.py
@@ -3,6 +3,6 @@ model = YOLO("yolov8n.yaml")
 if __name__ == '__main__':
     model.train(data="data.yaml",
                 mode="detect",
-                epochs=100,
+                epochs=10,
                 imgsz=640,
                 device="0")
