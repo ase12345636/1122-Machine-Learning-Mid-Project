35c67d9b338a1bee0b3f0c944fa632d0fdbcb087
ase12345636 <ase12345636@gmail.com>
2024-04-19 19:17:58 +0800

    Now progress

--- yolov8.py
+++ yolov8.py
@@ -7,17 +7,7 @@ if __name__ == '__main__':
                 epochs=20,
                 imgsz=640,
                 device="0")
-    val_his = model.val()
 
-from ultralytics import YOLO
-model = YOLO("yolov8n.yaml")
-if __name__ == '__main__':
-    model.train(data="data.yaml",
-                mode="detect",
-                epochs=10,
-                imgsz=640,
-                device="0")
-    
     model = YOLO("runs\\detect\\train\\weights\\best.pt")
     result = model.predict(source="dataset/valid/images",
         mode="predict",
