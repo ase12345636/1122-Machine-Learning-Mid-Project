544948582069215bcf20aec3b4b015c37fc0ea01
ase12345636 <ase12345636@gmail.com>
2024-04-19 18:31:00 +0800

    Now progress

--- yolov8.py
+++ yolov8.py
@@ -1,8 +1,10 @@
 from ultralytics import YOLO
-model = YOLO("yolov8n.yaml")
+model = YOLO("yolov8n.pt")
 if __name__ == '__main__':
     model.train(data="data.yaml",
                 mode="detect",
-                epochs=100,
+                batch=256,
+                epochs=20,
                 imgsz=640,
                 device="0")
+    val_his = model.val()
