f207c964782ade5cc86b372be127373184fbb5fe
YiYuan <yiyuan0918@gmail.com>
2024-04-21 01:20:46 +0800

    Add yolov5 training result ( weight, loss, predict .etc)

--- yolov5/yolov5_train.py
+++ yolov5/yolov5_train.py
@@ -1,10 +1,9 @@
 from ultralytics import YOLO
-model = YOLO('yolov5n.pt')
+model = YOLO('yolov5nu.pt')
 
 if __name__ == '__main__':
     model.train(data="data.yaml",
                 mode="detect",
                 epochs=50,
                 batch=-1,
-                imgsz=640,
-                device="0")
+                imgsz=640)
